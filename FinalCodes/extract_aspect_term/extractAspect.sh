dc000a9b4ce878a4f88bd128edfc5b08a3af14bb
pnisarg <pnisarg26@yahoo.com>
2017-04-25 14:50:05 -0700

    restructured

--- FinalCodes/extract_aspect_term/extractAspect.sh
+++ FinalCodes/extract_aspect_term/extractAspect.sh
@@ -1,7 +1,7 @@
-python aspectTermMarking.py termTrain.csv termTrain.txt output.txt
-CRF++-0.58/crf_learn template_1.txt output.txt model
-CRF++-0.58/crf_test -m model termTest.txt > result.txt
-python aspectTermEvaluation.py termTest.csv result.txt evaluation.txt
-python evaluate.py evaluation.txt aspectTerm.csv 
+python aspectTermMarking.py termTrain.csv termTrain.txt output/output.txt
+CRF++-0.58/crf_learn template.txt output/output.txt output/model
+CRF++-0.58/crf_test -m output/model termTest.txt > output/result.txt
+python aspectTermEvaluation.py termTest.csv output/result.txt output/evaluation.txt
+python evaluate.py output/evaluation.txt aspectTerm.csv 
 
 
