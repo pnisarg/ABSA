5077c2737b02652dabe7e6f394f558907c4ec79e
pnisarg <pnisarg26@yahoo.com>
2017-04-28 19:51:12 -0700

    repository clean up. Commiting final files

--- FinalCodes/extract_aspect_term/extractAspect.sh
+++
@@ -1,7 +0,0 @@
-python aspectTermMarking.py termTrain.csv termTrain.txt output/output.txt
-CRF++-0.58/crf_learn template.txt output/output.txt output/model
-CRF++-0.58/crf_test -m output/model termTest.txt > output/result.txt
-python aspectTermEvaluation.py termTest.csv output/result.txt output/evaluation.txt
-python evaluation.py output/evaluation.txt aspectTerm.csv
-
-
--- FinalCodes/extract_aspect_term/install.sh
+++
@@ -1,3 +0,0 @@
-./CRF++-0.58/configure
-./CRF++-0.58/make
-./sudo make install
