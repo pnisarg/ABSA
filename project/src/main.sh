4e97266747585f79c3e3f9a3272313c15e86d84e
pnisarg <pnisarg26@yahoo.com>
2017-04-28 19:45:20 -0700

    adding output for ate

--- project/src/main.sh
+++ project/src/main.sh
@@ -1,5 +1,12 @@
-python src/ate_crf_learn.py data/train/term_train.csv data/processed/dependency_parse_train.txt output/ate_learn.txt
-tools/CRF++-0.58/crf_learn src/ate_crf_features.txt output/ate_learn.txt output/ate_crf_model
-tools/CRF++-0.58/crf_test -m output/ate_crf_model data/test/term_test.txt > output/ate_result_parse.txt
-python src/ate_crf_detect.py data/test/term_test.csv output/ate_result_parse.txt output/ate_result_map.txt
-python src/ate_evaluation.py output/ate_result_map.txt data/raw/aspect_term.csv
+trainPath='./../data/train'
+testPath='./../data/test'
+pdataPath='./../data/processed'
+outputPath='./../output'
+toolsPath='./../tools'
+rawData='./../data/raw'
+
+python ate_crf_learn.py $trainPath/term_train.csv $pdataPath/dependency_parse_train.txt $outputPath/ate_learn.txt
+$toolsPath/CRF++-0.58/crf_learn ate_crf_features.txt $outputPath/ate_learn.txt  $outputPath/ate_crf_model
+$toolsPath/CRF++-0.58/crf_test -m  $outputPath/ate_crf_model $pdataPath/dependency_parse_test.txt >  $outputPath/ate_result_parse.txt
+python ate_crf_detect.py $testPath/term_test.csv  $outputPath/ate_result_parse.txt  $outputPath/ate_result_map.txt
+python evaluation/ate_evaluation.py  $outputPath/ate_result_map.txt $rawData/aspect_term.csv
